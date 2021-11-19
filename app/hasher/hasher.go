2492eff1f7b3d3390628ec92b2d79a79b3e8fdd6
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2021-11-19 15:55:24 +0300

    Create new directory app

---
+++ app/hasher/hasher.go
@@ -0,0 +1,40 @@
+package hasher
+
+import (
+	"errors"
+	"math"
+	"strings"
+)
+
+const (
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+	length = uint64(len(alphabet))
+	lengthNewUrl = 10
+)
+
+// Encode number - id of the new URL
+func Encode(number uint64) string {
+	var encodedBuilder strings.Builder
+	encodedBuilder.Grow(lengthNewUrl)
+
+	for ; number > 0; number = number / length {
+		encodedBuilder.WriteByte(alphabet[(number % length)])
+	}
+
+	return encodedBuilder.String()
+}
+
+func Decode(encoded string) (uint64, error) {
+	var number uint64
+
+	for i, symbol := range encoded {
+		alphabeticPosition := strings.IndexRune(alphabet, symbol)
+
+		if alphabeticPosition == -1 {
+			return uint64(alphabeticPosition), errors.New("invalid character: " + string(symbol))
+		}
+		number += uint64(alphabeticPosition) * uint64(math.Pow(float64(length), float64(i)))
+	}
+
+	return number, nil
+}
