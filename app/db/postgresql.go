9214296ee8edf4fb46e424b0f3041df0381470f7
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2021-11-22 13:30:43 +0300

    Fix postgre init

--- app/db/postgresql.go
+++ app/db/postgresql.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"os"
+	"strconv"
 )
 
 type PostgreSQL struct {
@@ -12,10 +13,16 @@ type PostgreSQL struct {
 }
 
 func InitPostgreSQL() (*PostgreSQL, error) {
-	fmt.Println("fd")
-	fmt.Println(os.Getenv("DATABASE_URL"))
-	fmt.Println("q")
-	pool, err := pgxpool.Connect(context.Background(), "postgres://postgres:postgres@localhost:5432/app")
+	dbUsername := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort, err := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	if err != nil {
+		dbPort = 5432
+	}
+	dbUri := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, dbUsername, dbName, dbPassword)
+	pool, err := pgxpool.Connect(context.Background(), dbUri)
 	if err != nil {
 		return nil, err
 	}
